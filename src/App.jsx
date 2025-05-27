ff383f3be979267fe9710d7b45b7936dbdd84558
Molly <162527916+Hyunsoo6257@users.noreply.github.com>
2025-05-27 19:48:24 +1000

    change package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 // src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfService from "./pages/TermsOfService";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename="/landing-page">
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/terms-of-service" element={<TermsOfService />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
