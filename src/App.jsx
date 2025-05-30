f8de7f6cb74601ef2ceed77d12a78fefdc51657a
Molly <162527916+Hyunsoo6257@users.noreply.github.com>
2025-05-30 11:41:46 +1000

    useRoutes error

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 // src/App.jsx
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Router, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfService from "./pages/TermsOfService";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-      <Route path="/terms-of-service" element={<TermsOfService />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/terms-of-service" element={<TermsOfService />} />
+      </Routes>
+    </Router>
   );
 }
 
